a6ab83280f977acff9cccf3b568b1004f2ef7c6d
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-25 16:50:04 -0700

    start multiple functions testing

--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,14 @@
 // Your code here
+const { assert } = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+const { reciprocal, returnsThree } = require('../problems/number-fun');
+
+describe('returnsThree', function () {
+  it('should return 3', function () {
+    const res = returnsThree();
+    assert.equal(res, 3);
+  });
+});
+
+describe('reciprocal', function () {});
