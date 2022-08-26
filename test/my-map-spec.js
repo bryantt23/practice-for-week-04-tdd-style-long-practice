a7632f1f34c4543932b660a49fba808166856d92
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-25 17:09:40 -0700

    use before each

--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -5,8 +5,13 @@ const expect = chai.expect;
 const myMap = require('../problems/my-map');
 
 describe('myMap', function () {
+  let array;
+  beforeEach(() => {
+    array = [1, 2, 3];
+  });
+
   it('should return doubled numbers array', function () {
-    const res = myMap([1, 2, 3], a => a * 2);
+    const res = myMap(array, a => a * 2);
     assert.deepEqual(res, [2, 4, 6]);
   });
 });
