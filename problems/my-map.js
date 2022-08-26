a6aafb9f0a8b5dc19184673906d282a91afdadb5
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-25 17:04:50 -0700

    map

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,10 @@
 function myMap(inputArray, callback) {
   // Your code here
+  const res = [];
+  inputArray.forEach(elem => {
+    res.push(callback(elem));
+  });
+  return res;
 }
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,12 @@
 // Your code here
+const { assert } = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+const myMap = require('../problems/my-map');
+
+describe('myMap', function () {
+  it('should return doubled numbers array', function () {
+    const res = myMap([1, 2, 3], a => a * 2);
+    assert.deepEqual(res, [2, 4, 6]);
+  });
+});
